f3dd38a86301a4ef031f0985e9f41174b8f523ed
Christian Georgeot <christian.georgeot@gmail.com>
2018-02-22 15:09:04 +0100

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Christian Georgeot on 22/02/2018.
+//  Copyright © 2018 Christian Georgeot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
